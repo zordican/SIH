105811188042a9a6b85eccf5f6f190ef95647ca9
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-09-08 16:27:10 +0530

    Initial Commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+const Hero = () => {
+  return (
+    <div>
+      This is the hero section.
+    </div>
+  )
+}
+
+export default Hero
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import React from 'react';
+import {
+  createBrowserRouter,RouterProvider
+} from "react-router-dom";
 import './index.css'
+import Hero from './Components/Hero/Hero.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  { path: "/",element: <Hero />,},
+  
+// { path: "/user/sharedcarts", element: <UserSharedCartsPage/>},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
