2efaed7dbb0a9912b6b57d391bc0793b9eec2642
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2024-09-12 23:04:50 +0530

    done_prot

--- src/Pages/Hero/Hero.jsx
+++ src/Pages/Hero/Hero.jsx
@@ -12,14 +12,14 @@ const Hero = () => {
 
       <div className={styles.description}>
         <h1>
-          <span className={styles.intelligence}>Intelligence</span> based <br /> model for <br />
-          <span className={styles.electricityDemand}>electricity demand</span> <br /> 
-          <span className={styles.projection}>projection</span> projection for Delhi <br /> Power System
+        <span className={styles.intelligence}>Intelligence</span> based <br /> model  for <br />
+        <span className={styles.electricityDemand}>electricity demand</span>  <br /> <span className={styles.projection}>projection</span> projection for Delhi <br /> Power System
         </h1>
         <button on onClick={handleNavigate}>GET STARTED</button>
       </div>
+      <div className={styles.imageContainer}><img src="https://res.cloudinary.com/dp3aoinmu/image/upload/v1725871913/Walmart_PNG/ii3xqqpgokfoltegbgpa.png" alt="" /></div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
