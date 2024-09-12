9a1541cfc452a37c92358b5c41c1766bc22e3bfd
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2024-09-12 23:21:22 +0530

    againfrontd

--- src/Pages/Hero/Chartii.jsx
+++ src/Pages/Hero/Chartii.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, registerables } from 'chart.js';
-import './Chartii.scss'; 
+import './chartii.scss'; 
 
 ChartJS.register(...registerables);
 
